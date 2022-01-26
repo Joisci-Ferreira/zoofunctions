d497aab077755adbee685a3b7613d22ae890e7bb
Joiscilene <joisci.lene@hotmail.com>
2022-01-26 00:52:26 -0300

    Adiciona requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => animal === specie.name).residents
+    .every((specie) => specie.age >= age);
 }
 
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
