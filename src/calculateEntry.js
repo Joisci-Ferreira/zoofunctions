a891cc0572d0d9acbab6f6a0b05ee2c9ef6ea498
Joiscilene <joisci.lene@hotmail.com>
2022-01-27 15:57:33 -0300

    Adiciona requisitos 3 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,30 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const idade = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      idade.child += 1;
+    }
+    if (entrant.age >= 18 && entrant.age < 50) {
+      idade.adult += 1;
+    }
+    if (entrant.age >= 50) {
+      idade.senior += 1;
+    }
+  });
+  return idade;
 }
 
 function calculateEntry(entrants) {
-
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const priceChild = countEntrants(entrants).child * prices.child;
+  const priceAdult = countEntrants(entrants).adult * prices.adult;
+  const priceSenior = countEntrants(entrants).senior * prices.senior;
+  return parseFloat((priceChild + priceAdult + priceSenior).toFixed(2));
 }
 
 module.exports = { calculateEntry, countEntrants };
