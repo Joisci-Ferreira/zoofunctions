30098fe274457f217ead8b0f6454888b7e88fb68
Joiscilene <joisci.lene@hotmail.com>
2022-01-31 01:12:10 -0300

    Adicionando requisitos 7 e 9

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,46 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function animalsLocation(locations) {
+  const animalsPerLocation = {};
+  locations.forEach((location) => {
+    const animals = species
+      .filter((animal) => animal.location === location)
+      .map((animal) => animal.name);
+    if (animals.length !== 0) animalsPerLocation[location] = animals;
+  });
+  return animalsPerLocation;
+}
+function animalsRecupere(locations, sorted, sex) {
+  const animalsLocationName = {};
+  locations.forEach((location) => {
+    const animals = species
+      .filter((animal) => animal.location === location).map((animal) => {
+        const nameKey = animal.name;
+        const nameValues = animal.residents.filter((resident) => {
+          const sexo = sex !== undefined;
+          return sexo ? resident.sex === sex : true;
+        })
+          .map((resident) => resident.name);
+        if (sorted) nameValues.sort();
+        return {
+          [nameKey]: nameValues,
+        };
+      });
+    animalsLocationName[location] = animals;
+  });
+  return animalsLocationName;
+}
 function getAnimalMap(options) {
-  // seu código aqui
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  if (!options) return animalsLocation(locations);
+  const {
+    includeNames,
+    sorted,
+    sex,
+  } = options;
+  if (!includeNames) return animalsLocation(locations);
+  return animalsRecupere(locations, sorted, sex);
 }
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getSpecie = employees.find((employee) => employee.id === id).responsibleFor;
+  return species.find((specie) => specie.id === getSpecie[0]).residents
+    .reduce((acc, animal) => {
+      const obj = Object.values(animal);
+      return acc[2] > obj[2] ? acc : obj;
+    }, []);
 }
 
 module.exports = getOldestFromFirstSpecies;
