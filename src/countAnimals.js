73a6e4b6d2d32e6cf828e12428723659d59b0507
Joiscilene <joisci.lene@hotmail.com>
2022-02-01 07:31:42 -0300

    Adicionando requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const result = {};
+    species.forEach(({ name, residents }) => {
+      result[name] = residents.length;
+    });
+    return result;
+  }
+
+  const animals = species.find((specie) => specie.name === animal.specie).residents;
+
+  return ('sex' in animal)
+    ? animals.filter((resident) => resident.sex === animal.sex).length
+    : animals.length;
 }
 
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
