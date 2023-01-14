2fe29b0086c35569fb0deb31a1a07d9c080ab5f4
Joiscilene <joisci.lene@hotmail.com>
2023-01-14 07:38:53 -0300

    adicionando correções

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function findWorker(id) {
+  return employees.find((employee) => employee.id === id
+    || employee.firstName === id
+    || employee.lastName === id);
+}
+
+const getSpeciesLocations = (specieIds) => specieIds.reduce((acc, specieId) => {
+  acc.push(species.find((animal) => animal.id === specieId).location);
+
+  return acc;
+}, []);
+
+const employeeInfo = ({ id, firstName, lastName, respponsibleFor }) => ({
+  id,
+  fullName: `${firstName} ${lastName}`,
+  species: getSpeciesLocations(respponsibleFor),
+  locations: getSpeciesLocations(respponsibleFor),
+});
+
+function getEmployeesCoverage(idOrName = null) {
+  if (!idOrName) {
+    return employees.reduce((acc, employee) => {
+      acc.push(employeeInfo(employee));
+
+      return acc;
+    }, []);
+  }
+
+  const worker = findWorker(idOrName[Object.keys(idOrName)[0]]);
+
+  if (!worker) throw new Error('Informações inválidas');
+  return employeeInfo(worker);
 }
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const openTime = (open, close) => {
+  if (open === 0) return 'CLOSED';
+
+  return `Open from ${open}am until ${close}pm`;
+};
+
+const exhibition = (weekDay) => {
+  const exhibitionAnimals = species.reduce((acc, specie) => {
+    if (specie.availability.includes(weekDay)) acc.push(specie.name);
+
+    return acc;
+  }, []);
+
+  return (exhibitionAnimals.length) ? exhibitionAnimals : 'The zoo will be closed!';
+};
+
+const daySchedule = (weekDay) => ({
+  [weekDay]: {
+    officeHour: openTime(hours[weekDay].open, hours[weekDay].close),
+    exhibition: exhibition(weekDay),
+  },
+});
+
+const fullSchedule = () => Object.keys(hours).reduce((acc, weekDay) =>
+  Object.assign(acc, daySchedule(weekDay)), {});
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animal = species.find((specie) => specie.name === scheduleTarget);
+
+  if (animal) return animal.availability;
+  if (Object.keys(hours).includes(scheduleTarget)) return daySchedule(scheduleTarget);
+
+  return fullSchedule();
 }
 
 module.exports = getSchedule;
